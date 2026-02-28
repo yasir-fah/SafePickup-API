563d596cc1036a298a2946e17175b2e5bb63e4be
dev <dev@test.com>
2026-03-01 00:50:08 +0300

    initial commit & project setup

---
+++ src/main/java/com/finalproject/safepickup/SafePickupApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.safepickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePickupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePickupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/safepickup/SafePickupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.safepickup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePickupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
